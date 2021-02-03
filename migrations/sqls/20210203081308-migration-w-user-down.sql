02c4a1690bcceddfc451fad4d9f4796751c7e98c
lilykr <lisa-lou.k@hotmail.fr>
2021-02-03 10:38:39 +0100

    added website in charity table

--- migrations/sqls/20210203081308-migration-w-user-down.sql
+++ migrations/sqls/20210203081308-migration-w-user-down.sql
@@ -1,1 +1,8 @@
 /* Replace with your SQL commands */
+DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `event`;
+DROP TABLE IF EXISTS `charity_has_category`;
+DROP TABLE IF EXISTS `category`;
+DROP TABLE IF EXISTS `charity`;
+
+
