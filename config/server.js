9af70aa4b8d280f10bafe132cbce3c699ff1c864
lilykr <lisa-lou.k@hotmail.fr>
2021-04-05 13:03:43 +0200

    keystone

--- config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'e90ee7ef7a1b39961bf734efe0d4fd46'),
-    },
-  },
-});
--- extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '95b4c2de-06cb-4126-85bf-3c9b2fe0c7ad'
-};
